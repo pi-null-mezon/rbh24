a4559b83a017435142b8d891cc15c1dc66ebd2f8
sasha <pi-null-mezon>
2024-10-11 15:34:32 +0300

    fix freeze in validation

--- train_adapter_for_instantid/validate.py
+++ train_adapter_for_instantid/validate.py
@@ -33,6 +33,7 @@ cfg.size_for_visualization = (256, 256)
 cfg.target_reconstruction_size = (512, 512)
 cfg.reconstruction_iterations = 11  # low values produce low quality results, high values took too long time
 cfg.max_samples_to_collect = args.max_ids  # we will take 1 sample per id
+cfg.max_allowed_attempts_to_generate_face = 4  # sometime diffusion could not generate face (wrong template projection)
 
 buffalo = FaceAnalysis(name='buffalo_l', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
 buffalo.prepare(ctx_id=0, det_size=(640, 640))
@@ -69,6 +70,7 @@ print("MEASUREMENTS COLLECTION - please wait...", flush=True)
 cosines = []
 for template, photo, filename in test_dataloader:
     ot = template.squeeze(0).cpu().numpy()
+    fail_iterations = 0
     face_not_detected = True
     while face_not_detected:
         pilimg = reconstruct_face_from_template(template=ot,
@@ -81,35 +83,41 @@ for template, photo, filename in test_dataloader:
         if info is not None:
             rnt = info['embedding'] / np.linalg.norm(info['embedding'])
             face_not_detected = False
+        fail_iterations += 1
+        if fail_iterations > cfg.max_allowed_attempts_to_generate_face:
+            break
+    if not face_not_detected:
+        ont = ot / np.linalg.norm(ot)
+        ont = ont.squeeze(0)
+        cosine = np.dot(rnt, ont)
+        cosines.append(cosine)
 
-    ont = ot / np.linalg.norm(ot)
-    ont = ont.squeeze(0)
-    cosine = np.dot(rnt, ont)
-    cosines.append(cosine)
+        # save generation locally
+        _id = filename[0].split('/', 1)[0]
+        os.makedirs(os.path.join(artifacts_path, _id))
+        pilimg.save(f'{os.path.join(artifacts_path, filename[0].rsplit(".",1)[0])}.jpg')
 
-    # save generation locally
-    _id = filename[0].split('/', 1)[0]
-    os.makedirs(os.path.join(artifacts_path, _id))
-    pilimg.save(f'{os.path.join(artifacts_path, filename[0].rsplit(".",1)[0])}.jpg')
+        if cfg.visualize:
+            orig = photo.squeeze(0).cpu().numpy()
+            orig = cv2.resize(orig, cfg.size_for_visualization, cv2.INTER_CUBIC)
+            rec = np.array(pilimg)
+            rec = cv2.cvtColor(rec, cv2.COLOR_RGB2BGR)
+            rec = crop_square_roi(rec, bbox_upscale(image=rec, bbox=info['bbox'], resize_factor=1.1), v2hshift=-0.075)
+            rec = cv2.resize(rec, cfg.size_for_visualization, cv2.INTER_CUBIC)
+            canvas = np.zeros(shape=(orig.shape[0], orig.shape[1] + rec.shape[1], orig.shape[2]), dtype=np.uint8)
+            canvas[0:orig.shape[0], 0:orig.shape[1]] = orig
+            canvas[0:rec.shape[0], orig.shape[1]:orig.shape[1] + rec.shape[1]] = rec
+            info = f"cosine: {cosine:.3f}"
+            color = (0, 255, 0) if cosine > cfg.buffalo_cosine_threshold else (0, 55, 255)
+            cv2.putText(canvas, info, (6, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(canvas, info, (4, 18), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2, cv2.LINE_AA)
+            cv2.imshow("probe", canvas)
+            cv2.waitKey(cfg.visualize_ms)
+        if len(cosines) >= cfg.max_samples_to_collect:
+            break
+    else:
+        cosines.append(0)
 
-    if cfg.visualize:
-        orig = photo.squeeze(0).cpu().numpy()
-        orig = cv2.resize(orig, cfg.size_for_visualization, cv2.INTER_CUBIC)
-        rec = np.array(pilimg)
-        rec = cv2.cvtColor(rec, cv2.COLOR_RGB2BGR)
-        rec = crop_square_roi(rec, bbox_upscale(image=rec, bbox=info['bbox'], resize_factor=1.1), v2hshift=-0.075)
-        rec = cv2.resize(rec, cfg.size_for_visualization, cv2.INTER_CUBIC)
-        canvas = np.zeros(shape=(orig.shape[0], orig.shape[1] + rec.shape[1], orig.shape[2]), dtype=np.uint8)
-        canvas[0:orig.shape[0], 0:orig.shape[1]] = orig
-        canvas[0:rec.shape[0], orig.shape[1]:orig.shape[1] + rec.shape[1]] = rec
-        info = f"cosine: {cosine:.3f}"
-        color = (0, 255, 0) if cosine > cfg.buffalo_cosine_threshold else (0, 55, 255)
-        cv2.putText(canvas, info, (6, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.putText(canvas, info, (4, 18), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2, cv2.LINE_AA)
-        cv2.imshow("probe", canvas)
-        cv2.waitKey(cfg.visualize_ms)
-    if len(cosines) >= cfg.max_samples_to_collect:
-        break
 
 cosines = np.array(cosines)
 with open(os.path.join(artifacts_path, f'{adapter_name}.cosines.pkl'), 'wb') as o_f:
